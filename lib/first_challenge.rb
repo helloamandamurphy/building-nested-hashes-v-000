5ffc0ede1c28c4147b779e64ef96575c6edf0079
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-07 20:20:28 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,24 +1,6 @@
 def first_challenge
    epic_tragedy = {
-     montague: {
-       patriarch: {
-         name: "Lord Montague",
-         :age => "53"
-       }
-       :matriarach => {
-         :name => "Lady Montague"
-         :age => "54"
-       }
-       :hero => {
-         :name => "Rom"
-       }
-       :hero_friends => []
-     },
-     :capulet => {
-       :patriarch => {}
-       :matriarach => {}
-       :heroine => {}
-       :heroine_friends => []
-     }
+     montague: {}
+     capulet: {}
    }
 end
